fe4dd8c7c88200d1afdfaa19450bb67201a948e2
michaeltout <toutonghi.michael@gmail.com>
2020-11-29 16:52:10 +0100

    Tweak get_conversion_paths api to include currencies reachable through via command

--- routes/api/native/getConversionPaths.js
+++ routes/api/native/getConversionPaths.js
@@ -6,33 +6,65 @@ const checkFlag = require('../utils/flags');
 module.exports = (api) => {    
   // Derives possible conversion paths between source and destination currencies
   // (or all possible destinations if destination is null)
-  api.native.get_conversion_paths = (chain, src, dest = null) => {
+  api.native.get_conversion_paths = (chain, src, dest = null, includeVia = false, ignoreCurrencies = [], via = null) => {
     return new Promise(async (resolve, reject) => {  
       try {
-        const source = await api.native.get_currency(chain, src)
+        const source = typeof src === 'string' ? await api.native.get_currency(chain, src) : src
 
-        api.native.callDaemon(chain, 'getcurrencyconverters', dest === null ? [src] : [src, dest])
-        .then(async (paths) => {
-          let convertables = {} 
+        api.native
+          .callDaemon(
+            chain,
+            "getcurrencyconverters",
+            dest === null ? [source.currencyid] : [source.currencyid, dest]
+          )
+          .then(async (paths) => {
+            let convertables = {};
 
-          paths.forEach(path => {
-            const currencyName = Object.keys(path)[0]
-            convertables[path[currencyName].currencyid] = path[currencyName]
-          })
+            paths.forEach((path) => {
+              const currencyName = Object.keys(path)[0];
+              convertables[path[currencyName].currencyid] = {
+                via,
+                destination: path[currencyName],
+              };
+            });
 
-          if (checkFlag(source.options, IS_FRACTIONAL) && dest == null) {
-            for (const reserve of source.currencies) {
-              if (!convertables[reserve]) {
-                convertables[reserve] = await api.native.get_currency(chain, reserve)
+            if (checkFlag(source.options, IS_FRACTIONAL) && dest == null) {
+              for (const reserve of source.currencies) {
+                if (
+                  !convertables[reserve] &&
+                  !ignoreCurrencies.includes(reserve)
+                ) {
+                  convertables[reserve] = {
+                    via,
+                    destination: await api.native.get_currency(chain, reserve),
+                  };
+                }
               }
             }
-          }
 
-          resolve(convertables)
-        })
-        .catch(err => {
-          reject(err)
-        })
+            if (includeVia) {
+              for (const key in convertables) {
+                if (!ignoreCurrencies.includes(key)) {
+                  convertables = {
+                    ...convertables,
+                    ...(await api.native.get_conversion_paths(
+                      chain,
+                      key,
+                      dest,
+                      false,
+                      Object.keys(convertables),
+                      convertables[key].destination
+                    )),
+                  };
+                }
+              }
+            }
+
+            resolve(convertables);
+          })
+          .catch((err) => {
+            reject(err);
+          });
       } catch(e) {
         reject(e)
       }
@@ -43,8 +75,9 @@ module.exports = (api) => {
     const coin = req.body.chainTicker;
     const src = req.body.src;
     const dest = req.body.dest;
+    const includeVia = req.body.includeVia
 
-    api.native.get_conversion_paths(coin, src, dest)
+    api.native.get_conversion_paths(coin, src, dest, includeVia)
     .then((paths) => {  
       res.send(JSON.stringify({
         msg: 'success',
